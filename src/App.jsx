bcc3f1f2e428156c8e31b3dde538ba1ee55d99fc
akareagwom <akareazikang20@gmail.com>
2024-11-03 21:44:49 +0100

    background

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-[#1e1f29]'>
-      <div className="text-white py-20 text-center bg-[url(images/bg-stars.svg)] h-full">
+    <div className='bg-[#1e1f29] h-[100vh]'>
+      <div className="text-white py-20 text-center h-[90vh] bg-[url(images/bg-stars.svg)] h-full">
         <h1 className='uppercase text-2xl text-bold'>We're launching soon</h1>
+        <div className="">
+        </div>
       </div>
+          <img src="images/pattern-hills.svg" alt="" srcset="" />
     </div>
   )
 }
