39b877adde28ae5f379eb798e2285a3f200e80fc
akareagwom <akareazikang20@gmail.com>
2024-11-04 21:27:49 +0100

    pattern

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='bg-[#1e1f29] h-[100vh]'>
-      <div className="text-white py-20 text-center h-[90vh] bg-[url(images/bg-stars.svg)] h-full">
-        <h1 className='uppercase text-2xl text-bold'>We're launching soon</h1>
+    <div className='bg-[#1e1f29] h-auto' >
+      <div className="text-white h-[60.3vh] py-20 text-center bg-[url(images/bg-stars.svg)] h-full">
+        <h1 className='uppercase  text-2xl text-bold'>We're launching soon</h1>
         <div className="">
         </div>
       </div>
-          <img src="images/pattern-hills.svg" alt="" srcset="" />
+          <img className='h-[40vh] w-[200%]' src="images/pattern-hills.svg" alt="" srcset="" />
     </div>
   )
 }
